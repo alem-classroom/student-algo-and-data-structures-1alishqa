239e3afff81e5525301e799ab25aab8597600bd8
1alishqa <1alishqa@gmail.com>
2020-11-20 15:44:38 +0600

    solved: linear-search

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -1,4 +1,18 @@
 def linear_search(lst, to_find):
+
+  count = 0
+
+  for i in lst:
+    if i == to_find:
+      return count
+      break
+    count = count + 1
+
+  return -1
+  
   # search for the element to_find inside lst
   # if found, return index of element
   # else return -1
+
+# print(linear_search(([5, 6, 'seven', 10]), 'seven'))
+# print(linear_search(([5, 6, 'seven', 10]), 'sevenTY'))
