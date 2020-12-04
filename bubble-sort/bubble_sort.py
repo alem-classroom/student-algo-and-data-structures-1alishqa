7dbbb0156f3f3bad6055e230e04948a2221c7144
1alishqa <1alishqa@gmail.com>
2020-12-04 16:40:30 +0600

    solved: bubble-sort

--- bubble-sort/bubble_sort.py
+++ bubble-sort/bubble_sort.py
@@ -1,2 +1,19 @@
 def bubble_sort(lst):
+
+    swapped = True
+    while swapped:
+
+        swapped = False
+
+        for i in range (len(lst) - 1):
+
+            if lst[i] > lst[i + 1]:
+
+                lst[i], lst[i + 1] = lst[i + 1], lst[i]
+                swapped = True
+
+    return lst    
     # sort the list via bubble sort
+
+# l = [5, 4, 3, 2, 1, 0, -1]
+# print(bubble_sort(l))
